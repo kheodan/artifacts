696357537e207bfdc7d55267f2428f730ea6e155
kheodan <kheodan@gmail.com>
2020-07-24 19:29:31 +0300

    check method for cache

--- python/cache.py
+++ python/cache.py
@@ -4,31 +4,48 @@ from collections import OrderedDict
 
 
 class Cache:
-    def __init__(self, size):
+    def __init__(self, size, ttl):
         self.cache = OrderedDict()
         self.cacheSize = size
+        self.ttl = ttl
 
     def __getitem__(self, key):
-        value = self.cache[key]
-        return self.touch(key, value)
+        try:
+            value = self.cache.get(key).get('value')
+            return self.touch(key, value)
+        except AttributeError:
+            return None
 
     def __setitem__(self, key, value):
         self.touch(key, value)
 
     def touch(self, key, value):
         try:
-            res = self.cache.pop(key)['value']
+            self.cache.pop(key)
             self.cache[key] = {'time': datetime.datetime.now(), 'value': value}
-            return res
+            return value
         except KeyError:
             self.cache[key] = {'time': datetime.datetime.now(), 'value': value}
             if len(self.cache.keys()) > self.cacheSize:
                 self.cache.popitem(last=False)
-                res = self.cache.pop(key)['value']
                 self.cache[key] = {'time': datetime.datetime.now(), 'value': value}
-                return res
+                return value
 
-# a = Cache(100)
+    def check(self, key, **kwargs):
+        ttl = kwargs.get('ttl', self.ttl)
+        res = self.cache.get(key, None)
+        if res is not None:
+            if res.get('time') > datetime.datetime.now()-datetime.timedelta(seconds=ttl):
+                return res.get('value')
+            else:
+                self.cache.pop(key)
+                return None
+        else:
+            return None
+
+# size = 128
+# default_ttl = 60
+# a = Cache(size,default_ttl)
 # a['foo'] = 'bar'
 # a.cache.get('foo').get('value')
 # > 'bar'
@@ -38,3 +55,9 @@ class Cache:
 #
 # a['foo']
 # > 'bar'
+#
+# a.check('foo',ttl=1500)
+# > None || 'bar'
+#
+# a.check('foo')
+# > None || 'bar'
